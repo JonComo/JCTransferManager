9bf7eb67072e5c9bc61f6b557badad188421cd43
Jon Como <jon@underground.net>
2013-05-23 13:06:46 -0700

    First Commit

---
+++ JCTransferManager/JCTransferManager.h
@@ -0,0 +1,26 @@
+//
+//  JCTransferManager.h
+//  Underground
+//
+//  Created by Jon Como on 5/8/13.
+//  Copyright (c) 2013 Jon Como. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class AmazonS3Client;
+
+typedef void (^ProgressBlock)(float percentage,int bytesUploaded, int bytesTotal);
+typedef void (^CompletionBlock)(BOOL success);
+
+@interface JCTransferManager : NSObject
+
+@property (nonatomic, strong) AmazonS3Client *client;
+
++(JCTransferManager *)sharedManager;
+
+-(void)authorizeWithKey:(NSString *)key secretKey:(NSString *)secretKey bucket:(NSString *)bucket;
+-(void)uploadData:(NSArray *)dataArray filenames:(NSArray *)filenames progress:(ProgressBlock)progress completion:(CompletionBlock)completion;
+-(void)endBackgroundProcess;
+
+@end
---
+++ JCTransferManager/JCTransferManager.m
@@ -0,0 +1,122 @@
+//
+//  JCTransferManager.m
+//  Underground
+//
+//  Created by Jon Como on 5/8/13.
+//  Copyright (c) 2013 Jon Como. All rights reserved.
+//
+
+#import "JCTransferManager.h"
+#import <AWSS3/AWSS3.h>
+#import <AWSS3/S3TransferManager.h>
+#import <AWSRuntime/AWSRuntime.h>
+
+@interface JCTransferManager () <AmazonServiceRequestDelegate>
+{
+    S3TransferManager *transferManager;
+    NSString *bucketName;
+    
+    ProgressBlock progressBlock;
+    CompletionBlock completionBlock;
+    
+    __block UIBackgroundTaskIdentifier background_task;
+    
+    int filesToUpload;
+}
+
+@end
+
+@implementation JCTransferManager
+
++(JCTransferManager *)sharedManager
+{
+    static JCTransferManager *sharedManager;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedManager = [[self alloc] init];
+    });
+    
+    return sharedManager;
+}
+
+-(void)authorizeWithKey:(NSString *)key secretKey:(NSString *)secretKey bucket:(NSString *)bucket
+{
+    self.client = [[AmazonS3Client alloc] initWithAccessKey:key withSecretKey:secretKey];
+    transferManager = [S3TransferManager new];
+    transferManager.delegate = self;
+    transferManager.s3 = self.client;
+    bucketName = bucket;
+}
+
+-(void)uploadData:(NSArray *)dataArray filenames:(NSArray *)filenames progress:(ProgressBlock)progress completion:(CompletionBlock)completion
+{
+    progressBlock = progress;
+    completionBlock = completion;
+    filesToUpload = dataArray.count;
+    
+    [self runInBackgroundIfPossible:^{
+        for (u_int i = 0; i<dataArray.count; i++){
+            [transferManager uploadData:dataArray[i] bucket:bucketName key:filenames[i]];
+        }
+    }];
+}
+
+-(void)runInBackgroundIfPossible:(void(^)(void))block
+{
+    if ([[UIDevice currentDevice] isMultitaskingSupported]) {
+        
+        background_task = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler: ^ {
+            [[UIApplication sharedApplication] endBackgroundTask: background_task];
+            background_task = UIBackgroundTaskInvalid;
+        }];
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            if (block) block();
+        });
+        
+        return;
+    }
+    
+    if (block) block();
+}
+
+-(void)endBackgroundProcess
+{
+    [[UIApplication sharedApplication] endBackgroundTask:background_task];
+    background_task = UIBackgroundTaskInvalid;
+}
+
+-(void)request:(AmazonServiceRequest *)request didReceiveResponse:(NSURLResponse *)response
+{
+}
+
+-(void)request:(AmazonServiceRequest *)request didReceiveData:(NSData *)data
+{
+}
+
+-(void)request:(AmazonServiceRequest *)request didSendData:(NSInteger)bytesWritten totalBytesWritten:(NSInteger)totalBytesWritten totalBytesExpectedToWrite:(NSInteger)totalBytesExpectedToWrite
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (progressBlock) progressBlock(((float)totalBytesWritten / (float)totalBytesExpectedToWrite) / (filesToUpload), totalBytesWritten, totalBytesExpectedToWrite);
+    });
+}
+
+-(void)request:(AmazonServiceRequest *)request didCompleteWithResponse:(AmazonServiceResponse *)response
+{
+    filesToUpload --;
+    
+    if (filesToUpload == 0){
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (completionBlock) completionBlock(YES);
+        });
+    }
+}
+
+-(void)request:(AmazonServiceRequest *)request didFailWithError:(NSError *)error
+{
+    if (completionBlock) completionBlock(NO);
+    [self endBackgroundProcess];
+}
+
+@end
